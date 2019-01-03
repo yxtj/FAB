86476447e61ffd8f6f31d80672de919fe1e41fd8
yxtj <yanxiangtianji@gmail.com>
2019-01-03 23:12:02 +0800

    add more activation functions

--- src/model/mathfunc.cpp
+++ src/model/mathfunc.cpp
@@ -11,3 +11,58 @@ double sigmoid_derivative(double y)
 	double t = sigmoid(y);
 	return t * (1.0 - t);
 }
+
+double relu(double x){
+	return x >= 0.0 ? x : 0;
+}
+
+double relu_derivative(double x){
+	return x >=0.0 ? 1 : 0;
+}
+
+double tanh_derivative(double y){
+	double t = tanh(y);
+	return 1 - t*t;
+}
+
+double arctan(double x){
+	return atan(x);
+}
+
+double arctan_derivative(double y){
+	return 1.0/(y*y + 1);
+}
+
+double softplus(double x){
+	return log1p(exp(x));
+}
+double softplus_derivative(double y){
+	return sigmoid(y);
+}
+
+std::vector<double> softmax(const std::vector<double>& x){
+	std::vector<double> res(x.size());
+	double sum=0.0;
+	for(size_t i=0;i<x.size();++i){
+		double t = exp(x[i]);
+		res[i]=t;
+		sum+=t;
+	}
+	for(double& v : res){
+		v/=sum;
+	}
+	return res;
+}
+
+std::vector<std::vector<double>> softmax_derivative(const std::vector<double>& y){
+	const size_t n = y.size();
+	std::vector<std::vector<double>> res(n, vector<double>(n));
+	for(size_t i=0;i<n;++i){
+		for(size_t j=0;j<n;++j)
+			if(i==j)
+				res[i][j]=y[i]*(1-y[j]);
+			else
+				res[i][j]=-y[i]*y[j];
+	}
+	return res;
+}
--- src/model/mathfunc.h
+++ src/model/mathfunc.h
@@ -1,4 +1,20 @@
 #pragma once
+#include <vector>
 
 double sigmoid(double x);
 double sigmoid_derivative(double y);
+
+double relu(double x);
+double relu_derivative(double y);
+
+//double tanh(double x);
+double tanh_derivative(double y);
+
+double arctan(double x);
+double arctan_derivative(double y);
+
+double softplus(double x);
+double softplus_derivative(double y);
+
+
+std::vector<double> softmax(const std::vector<double>& y);
