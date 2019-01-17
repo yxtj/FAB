8adfb9bc64f1090505d40fbe8c90edbbc2549bd1
yxtj <yanxiangtianji@gmail.com>
2019-01-17 13:41:17 -0500

    update run script

--- run.sh
+++ run.sh
@@ -7,14 +7,20 @@ LOGBASEDIR=~/Code/FSB/log/
 
 ALG=lr
 PARAM=10
+PARAMNAME=10
 
 #ALG=mlp
 #PARAM=10,15,1
+#PARAMNAME=10,15,1
+
+ALG=CNN
+PARAM=100-20,c,5-1,a,relu-1,p,max,2-1,c,5-1,a,relu,1,p,max,2-1
+PARAMNAME=100,20c5p2,1c5p2,1
 
 YLIST=10
 
 DSIZE=100k
-#DF=$ALG-$PARAM-$DSIZE-d.csv
+#DF=$ALG-$PARAMNAME-$DSIZE-d.csv
 
 BS=100 # 1000 10000
 LR=0.1 # 0.01
@@ -22,10 +28,10 @@ ITER=100k
 TIME=60
 
 function set_dir(){
-  export RESDIR=$RESBASEDIR/$PARAM-$DSIZE/$BS-$LR
-  export SCRDIR=$SCRBASEDIR/$PARAM-$DSIZE/$BS-$LR
-  export LOGDIR=$LOGBASEDIR/$PARAM-$DSIZE/$BS-$LR
-  mkdir -p $RESDIR
+  export RESDIR=$RESBASEDIR/$PARAMNAME-$DSIZE/$BS-$LR
+  export SCRDIR=$SCRBASEDIR/$PARAMNAME-$DSIZE/$BS-$LR
+  export LOGDIR=$LOGBASEDIR/$PARAMNAME-$DSIZE/$BS-$LR
+  mkdir -p $RESDIRls
   mkdir -p $SCRDIR
   mkdir -p $LOGDIR
 }
@@ -33,15 +39,16 @@ function set_dir(){
 set_dir
 
 cd ~/Code/FSB/lr-cpp/build
+cd ~/Code/FSB/FAB/build
 
 for i in 1 2 4 8; do for m in $MODE; do echo $i-$m -- $(date);
-mpirun -n $((i+1)) src/main/main $m $ALG $PARAM $DATADIR/$ALG-$PARAM-$DSIZE-d.csv $RESDIR/$m-$i.csv -1 $YLIST 0 $LR $BS $ITER $TIME 1000 0.5 200 --v=1 > $LOGDIR/$m-$i;
+mpirun -n $((i+1)) src/main/main $m $ALG $PARAM $DATADIR/$ALG-$PARAMNAME-$DSIZE-d.csv $RESDIR/$m-$i.csv -1 $YLIST 0 $LR $BS $ITER $TIME 1000 0.5 200 --v=1 > $LOGDIR/$m-$i;
 done done
 
 for PARAM in 10,1; do echo $PARAM;
 for BS in 10000 1000 100; do for LR in 0.1 0.01; do echo $BS - $LR; set_dir;
 for i in 1 2 4 8; do for m in $MODE; do echo $i-$m -- $(date);
-mpirun -n $((i+1)) src/main/main $m $ALG $PARAM $DATADIR/$ALG-$PARAM-$DSIZE-d.csv $RESDIR/$m-$i.csv -1 $YLIST 0 $LR $BS $ITER $TIME 1000 0.5 200 --v=2 > $LOGDIR/$m-$i;
+mpirun -n $((i+1)) src/main/main $m $ALG $PARAM $DATADIR/$ALG-$PARAMNAME-$DSIZE-d.csv $RESDIR/$m-$i.csv -1 $YLIST 0 $LR $BS $ITER $TIME 1000 0.5 200 --v=2 > $LOGDIR/$m-$i;
 done done
 done done
 done
