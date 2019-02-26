2cbe9c0f7aefd09fff906b14a0ed3bbaf31153b8
Tian Zhou <yanxiangtianji@gmail.com>
2019-02-25 23:35:13 -0500

    update run.sh with new changes

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-MODE="sync async fsb fab"
+MODE="bsp tap fsp aap"
 
 DATADIR=/tmp/tzhou/data
 RESBASEDIR=~/Code/FSB/result/
@@ -7,30 +7,30 @@ LOGBASEDIR=~/Code/FSB/log/
 
 ALG=lr
 PARAM=10
-PARAMNAME=10
 
 #ALG=mlp
 #PARAM=10,15,1
-#PARAMNAME=10,15,1
 
-ALG=CNN
-PARAM=100-20,c,5-1,a,relu-1,p,max,2-1,c,5-1,a,relu-1,p,max,2-1
-PARAMNAME=100,20c5p2,1c5p2,1
+#ALG=cnn
+#PARAM=100,20c5p2,1c5p2,1
+
+#ALG=rnn
+#PARAM=100,10rs5,1
 
 YLIST=10
 
 DSIZE=100k
-#DF=$ALG-$PARAMNAME-$DSIZE-d.csv
+#DF=$ALG-$PARAM-$DSIZE-d.csv
 
-BS=100 # 1000 10000
-LR=0.1 # 0.01
+bs=100 # 1000 10000
+lr=0.1 # 0.01
 ITER=100k
 TIME=60
 
 function set_dir(){
-  export RESDIR=$RESBASEDIR/$PARAMNAME-$DSIZE/$BS-$LR
-  export SCRDIR=$SCRBASEDIR/$PARAMNAME-$DSIZE/$BS-$LR
-  export LOGDIR=$LOGBASEDIR/$PARAMNAME-$DSIZE/$BS-$LR
+  export RESDIR=$RESBASEDIR/$ALG/$PARAM-$DSIZE/$bs-$lr
+  export SCRDIR=$SCRBASEDIR/$ALG/$PARAM-$DSIZE/$bs-$lr
+  export LOGDIR=$LOGBASEDIR/$ALG/$PARAM-$DSIZE/$bs-$lr
   mkdir -p $RESDIR
   mkdir -p $SCRDIR
   mkdir -p $LOGDIR
@@ -41,22 +41,30 @@ set_dir
 cd ~/Code/FSB/FAB/build
 
 for i in 1 2 4 8; do for m in $MODE; do echo $i-$m -- $(date);
-mpirun -n $((i+1)) src/main/main $m $ALG $PARAM $DATADIR/$ALG-$PARAMNAME-$DSIZE-d.csv $RESDIR/$m-$i.csv -1 $YLIST 0 $LR $BS $ITER $TIME 1000 0.5 200 --v=1 > $LOGDIR/$m-$i;
+mpirun -n $((i+1)) src/main/main $m $ALG $PARAM $DATADIR/$ALG-$PARAM-$DSIZE-d.csv $RESDIR/$m-$i.csv -1 $YLIST 0 $lr $bs $ITER $TIME 1000 0.5 200 portion:0.05 --v=1 > $LOGDIR/$m-$i;
 done done
 
 for PARAM in 10,1; do echo $PARAM;
-for BS in 10000 1000 100; do for LR in 0.1 0.01; do echo $BS - $LR; set_dir;
+for bs in 10000 1000 100; do for lr in 0.1 0.01; do echo $bs - $lr; set_dir;
 for i in 1 2 4 8; do for m in $MODE; do echo $i-$m -- $(date);
-mpirun -n $((i+1)) src/main/main $m $ALG $PARAM $DATADIR/$ALG-$PARAMNAME-$DSIZE-d.csv $RESDIR/$m-$i.csv -1 $YLIST 0 $LR $BS $ITER $TIME 1000 0.5 200 --v=1 > $LOGDIR/$m-$i;
+mpirun -n $((i+1)) src/main/main $m $ALG $PARAM $DATADIR/$ALG-$PARAM-$DSIZE-d.csv $RESDIR/$m-$i.csv -1 $YLIST 0 $lr $bs $ITER $TIME 1000 0.5 200 portion:0.05 --v=1 > $LOGDIR/$m-$i;
 done done
 done done
 done
 
+# evaluate
 PATH=$PATH:/home/tzhou/Code/FSB/FAB/build/Release/src/main
 
 for i in 1 2 4 8; do for m in $MODE; do echo $i-$m -- $(date);
-postprocess $ALG $PARAM  $RESDIR/$m-$i.csv $DATADIR/$ALG-$PARAMNAME-$DSIZE-d.csv -1 $YLIST $DATADIR/$ALG-$PARAMNAME-$DSIZE-p.txt $SCRDIR/$m-$i.txt 0 0
+postprocess $ALG $PARAM  $RESDIR/$m-$i.csv $DATADIR/$ALG-$PARAM-$DSIZE-d.csv -1 $YLIST $DATADIR/$ALG-$PARAM-$DSIZE-p.txt $SCRDIR/$m-$i.txt 0 0
 done done
 
+# calculate and evaluation (in background)
+PARAM=10,4r3,1;bs=3000;lr=0.1;
+for i in 1 2 4 8; do for m in $MODE; do echo $i-$m -- $(date); set_dir
+mpirun -n $((i+1)) src/main/main $m $ALG $PARAM $DATADIR/$ALG-$PARAM-$DSIZE-d.csv $RESDIR/$m-$i.csv -1 $YLIST 0 $lr $bs $ITER $TIME 1000 0.5 200 portion:0.05 --v=1 > $LOGDIR/$m-$i;
+echo "  postprocess";
+./Release/src/main/postprocess $ALG $PARAM  $RESDIR/$m-$i.csv $DATADIR/$ALG-$PARAM-$DSIZE-d.csv -1 $YLIST $DATADIR/$ALG-$PARAM-$DSIZE-p.txt $SCRDIR/$m-$i.txt 0 0 &
+done done
 
 
