c2d6ff359eb2945a62c7f6df819c96a9bd89741d
Tian Zhou <yanxiangtianji@gmail.com>
2019-03-25 06:04:33 -0400

    use new parameter in the running script

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 MODE="bsp tap fsp aap"
 
 DATADIR=/tmp/tzhou/data
+DATADIR=/var/tzhou/data
 RESBASEDIR=~/Code/FSB/result/
 SCRBASEDIR=~/Code/FSB/score/
 LOGBASEDIR=~/Code/FSB/log/
@@ -48,13 +49,13 @@ set_dir
 cd ~/Code/FSB/FAB/build
 
 for i in 1 2 4 8; do for m in $MODE; do echo $i-$m -- $(date);
-mpirun -n $((i+1)) src/main/main $m $ALG $PARAM $DATADIR/$ALG-$PARAM-$DSIZE-d.csv $RESDIR/$m-$i.csv -1 $YLIST 0 $lr $bs $ITER $TIME 1000 0.5 200 portion:0.05 all --v=1 > $LOGDIR/$m-$i;
+mpirun -n $((i+1)) src/main/main -m $m -a $ALG -p $PARAM -d $DATADIR/$ALG-$PARAM-$DSIZE-d.csv -o $RESDIR/$m-$i.csv -y $YLIST -l $lr -s $bs --term_iter $ITER --term_time $TIME --arch_iter 1000 --arch_time 0.5 --log_iter 200 --v=1 > $LOGDIR/$m-$i;
 done done
 
 for PARAM in 10,1; do echo $PARAM;
 for bs in 10000 1000 100; do for lr in 0.1 0.01; do echo $bs - $lr; set_dir;
 for i in 1 2 4 8; do for m in $MODE; do echo $i-$m -- $(date);
-mpirun -n $((i+1)) src/main/main $m $ALG $PARAM $DATADIR/$ALG-$PARAM-$DSIZE-d.csv $RESDIR/$m-$i.csv -1 $YLIST 0 $lr $bs $ITER $TIME 1000 0.5 200 portion:0.05 all --v=1 > $LOGDIR/$m-$i;
+mpirun -n $((i+1)) src/main/main -m $m -a $ALG -p $PARAM -d $DATADIR/$ALG-$PARAM-$DSIZE-d.csv -o $RESDIR/$m-$i.csv -y $YLIST -l $lr -s $bs --term_iter $ITER --term_time $TIME --arch_iter 1000 --arch_time 0.5 --log_iter 200 --v=1 > $LOGDIR/$m-$i;
 done done
 done done
 done
@@ -63,15 +64,14 @@ done
 PATH=$PATH:/home/tzhou/Code/FSB/FAB/build/Release/src/main
 
 for i in 1 2 4 8; do for m in $MODE; do echo $i-$m -- $(date);
-postprocess $ALG $PARAM  $RESDIR/$m-$i.csv $DATADIR/$ALG-$PARAM-$DSIZE-d.csv -1 $YLIST $DATADIR/$ALG-$PARAM-$DSIZE-p.txt $SCRDIR/$m-$i.txt 0 0
+postprocess -a $ALG -p $PARAM -r $RESDIR/$m-$i.csv -d $DATADIR/$ALG-$PARAM-$DSIZE-d.csv -y $YLIST --reference $DATADIR/$ALG-$PARAM-$DSIZE-p.txt -o $SCRDIR/$m-$i.txt
 done done
 
 # calculate and evaluation (in background)
 PARAM=10,4r3,1;bs=3000;lr=0.1;
 for i in 1 2 4 8; do for m in $MODE; do echo $i-$m -- $(date); set_dir
-mpirun -n $((i+1)) src/main/main $m $ALG $PARAM $DATADIR/$ALG-$PARAM-$DSIZE-d.csv $RESDIR/$m-$i.csv -1 $YLIST 0 $lr $bs $ITER $TIME 1000 0.5 200 portion:0.05 all --v=1 > $LOGDIR/$m-$i;
+mpirun -n $((i+1)) src/main/main -m $m -a $ALG -p $PARAM -d $DATADIR/$ALG-$PARAM-$DSIZE-d.csv -o $RESDIR/$m-$i.csv -y $YLIST -l $lr -s $bs --term_iter $ITER --term_time $TIME --arch_iter 1000 --arch_time 0.5 --log_iter 200 --v=1 > $LOGDIR/$m-$i;
 echo "  postprocess";
-./Release/src/main/postprocess $ALG $PARAM  $RESDIR/$m-$i.csv $DATADIR/$ALG-$PARAM-$DSIZE-d.csv -1 $YLIST $DATADIR/$ALG-$PARAM-$DSIZE-p.txt $SCRDIR/$m-$i.txt 0 0 &
+postprocess -a $ALG -p $PARAM -r $RESDIR/$m-$i.csv -d $DATADIR/$ALG-$PARAM-$DSIZE-d.csv -y $YLIST --reference $DATADIR/$ALG-$PARAM-$DSIZE-p.txt -o $SCRDIR/$m-$i.txt &
 done done
-
 
