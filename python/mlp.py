24acfb57b9b84fc4412df85d7cbb8c44f8b595c0
Tian Zhou <yanxiangtianji@gmail.com>
2019-04-18 00:34:35 -0400

    update python script for mlp

--- python/mlp.py
+++ python/mlp.py
@@ -2,21 +2,29 @@
 
 import numpy as np
 
-class NeuralNetwork(object):
+class MLP(object):
     def __init__(self, layers=[2,3,1], lrate=0.5):
         self.lrate = lrate
         self.layers = layers
-        #parameters
-        self.inputSize = 2
-        self.hiddenSize = 3
-        self.outputSize = 1
+        self.w = []
+        self.b = []
+        self.nw = []
+        for i in range(len(self.layers) - 1):
+            self.nw.append((layers[i]+1)*layers[i+1])
     
-        #weights
-        self.W1 = np.random.randn(self.inputSize, self.hiddenSize) # (3x2) weight matrix from input to hidden layer
-        self.W2 = np.random.randn(self.hiddenSize, self.outputSize) # (3x1) weight matrix from hidden to output layer
+    def initParam(self):
         self.w=[]
-        for i in range(len(layers)-1):
-            self.w.append(np.random.randn(layers[i], layers[i+1]))
+        for i in range(len(self.layers)-1):
+            self.w.append(np.random.randn(self.layers[i], self.layers[i+1]))
+            self.b.append(np.random.randn(self.layers[i+1]))
+
+    def setParam(self, param):
+        f=0
+        l=self.nw[0]
+        for i in range(len(self.layers) - 1):
+            self.w[i] = param[f:l]
+            f=l
+            l=l+self.nw[i]
 
     def forward(self, X):
         #forward propagation through our network
@@ -30,24 +38,23 @@ class NeuralNetwork(object):
         last = X
         self.output.append(last)
         for i in range(len(self.layers)-1):
-            mid = np.dot(last, self.w[i])
+            mid = np.dot(last, self.w[i]) + self.b[i]
             last = self.sigmoid(mid)
             self.output.append(last)
-        return o 
+        return last
 
     def backward(self, X, y, o):
         # backward propgate through the network
         o_error = o - y # error in output
         o_delta = o_error*self.sigmoidPrime(o) # applying derivative of sigmoid to error
+        self.grad2 = np.dot(self.z2.T, o_delta)
 
         z2_error = np.dot(o_delta, self.W2.T) # z2 error: how much our hidden layer weights contributed to output error
         z2_delta = z2_error*self.sigmoidPrime(self.z2) # applying derivative of sigmoid to z2 error
+        self.grad1 = np.dot(X.T, z2_delta)
 
-        self.W2 += -self.lrate * np.dot(self.z2.T, o_delta) # adjusting second set (hidden --> output) weights
-        self.W1 += -self.lrate * np.dot(X.T, z2_delta) # adjusting first set (input --> hidden) weights
         return
         # generalize
-        delta = np.array()
         for i in range(len(self.layers)-2, -1, -1):
             pred = self.output[i+1]
             if i == len(self.layers) - 2:
@@ -55,9 +62,15 @@ class NeuralNetwork(object):
             else:
                 error = np.dot(delta, self.w[i+1].T)
             delta = error * self.sigmoidPrime(pred)
-            grad = np.dot(self.output[i].T, delta)
-            self.w[i] += -self.lrate * grad
-        
+            self.g[i] = np.dot(self.output[i].T, delta)
+
+    def update(self):
+        self.W2 += -self.lrate * self.grad2 # adjusting second set (hidden --> output) weights
+        self.W1 += -self.lrate * self.grad1 # adjusting first set (input --> hidden) weights
+        return
+        for i in range(len(self.layers)-2, -1, -1):
+            self.w[i] += -self.lrate * self.g[i]
+
     def train(self, X, y):
         o = self.forward(X)
         self.backward(X, y, o)
@@ -83,7 +96,7 @@ def main():
     # scale units
     X = X/np.amax(X, axis=0) # maximum of X array
     y = y/100 # max test score is 100
-    nn = NeuralNetwork()
+    nn = MLP()
     for i in range(1000): # trains the NN 1,000 times
         if i % 10 == 0:
             p=nn.forward(X)
